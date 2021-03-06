73c7a2bd64daeb47d97b05b32c801a493164e99f
Shikhar <Shikhar.Sundriyal@gmail.com>
2021-03-07 01:25:45 +0530

    Activity1

--- Assignment1/activity/commands/view.js
+++ Assignment1/activity/commands/view.js
@@ -1,6 +1,7 @@
 function viewHelper(dirname, mode){
     if(mode == "tree"){
         console.log("tree view will be shown for ",dirname);
+        viewTree(dirname,"");
     }else if(mode == "flat"){
         console.log("flat structure will be shown for",dirname);
         viewFlat(dirname);
@@ -8,6 +9,7 @@ function viewHelper(dirname, mode){
 }
 
 let fs = require("fs");
+let path = require("path")
 
 function isFile(dir){
     //returns true or false
@@ -16,6 +18,22 @@ function isFile(dir){
 function getContent(dir){
     //returns an array of string
     return fs.readdirSync(dir);
+}
+function viewTree(src, indent){
+
+    let isfile = isFile(src);
+    if(isfile){
+        console.log(indent,path.basename(src));
+    }else{
+        console.log(indent,path.basename(src));
+        let fDirnames = getContent(src);
+
+        for(let i=0; i<fDirnames.length; i++){
+            let child = fDirnames[i];
+            let dirNamepath = path.join(src,child);
+            viewTree(dirNamepath,indent+"\t");
+        }
+    }
 }
 function viewFlat(dirname){
     let isfile = isFile(dirname);
