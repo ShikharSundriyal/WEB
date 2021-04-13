147c7b3275d38445140e49c0a232d7ddd99363d7
Shikhar <Shikhar.Sundriyal@gmail.com>
2021-04-13 20:22:22 +0530

    puppet

--- Puppeteer/puppet.js
+++ Puppeteer/puppet.js
@@ -1,23 +1,29 @@
 const puppeteer = require("puppeteer");
-
+let page;
 let browseropenP = puppeteer.launch({
     headless:false
 });
 console.log("before");
-browseropenP.then(function (browser) {
-    
-    console.log("browser opened");
-    
-    let alltabsPromise = browser.pages();
-    
-    alltabsPromise.then(function (tabs){
-        let page = tabs[0];
-        
-        let googlehomePageOpenPromise = page.goto("https://www.google.com/");
-
-        googlehomePageOpenPromise.then(function (){
-            console.log("google home page opened");
-        })
-    } )
-});
+browseropenP
+    .then(function (browser) {
+        console.log("browser opened");
+        let alltabsPromise = browser.pages(); // get all pages
+        alltabsPromise
+            .then(function (tabs){
+                page = tabs[0];
+                let googlehomePageOpenPromise = page.goto("https://www.google.com/"); // goto google.com
+                googlehomePageOpenPromise 
+                    .then(function (){
+                        console.log("mdn home page opened");
+                        function fn(){
+                            return document.querySelector(".gb_4.gb_5.gb_ae.gb_ce.gb_4c").innerText
+                        }
+                        let resPromise = page.evaluate(fn);
+                        resPromise
+                            .then(function (val){
+                            console.log(val);
+                        })
+                    })
+            })
+    });
 console.log("after");
